97a4c081e484131d7eece4e64f8a6d7c2948581c
shalu2022 <frontendstuff2022@gmail.com>
2024-05-13 19:30:34 +0530

    improved code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import util from 'util'
 
 const userController = {
     register : async (req, res, next)=>{
@@ -8,24 +9,24 @@ const userController = {
             const {firstName, lastName, email, password, confirmPassword,tc} = req.body
             const userExist = await User.findOne({email: email})
              if(!!userExist){
-                res.json({status: "failed", message: "User Already Exist"})
-            }else{
-                if(!firstName || !lastName || !email || !password || !confirmPassword || !tc){
-                    res.json({status: "failed", message: "Please provide required details"})
-                    return
-                }
-                if(password !== confirmPassword){
-                    res.json({status: "failed", message: "Confirm password and Password do not match"})
-                    return
-                }
-                let user = await User.create({firstName,lastName, email, password, tc})
-                //Generate Token
-                if(!!user){
-                    const token = jwt.sign({userId: user._id}, req.app.get('secretKey'), {expiresIn: '30m'})
-                    user.password = undefined
-                    if(token){                        
-                        res.status(201).json({status: "success", message: "User Created Successfully", data: user, token: token})
-                    }
+                return res.status(400).json({status: "failed", message: "User Already Exist"})
+            }
+            if(!firstName || !lastName || !email || !password || !confirmPassword || !tc){
+                return res.status(400).json({status: "failed", message: "Please provide required details"})    
+            }
+            if(password !== confirmPassword){
+                return res.status(400).json({status: "failed", message: "Confirm password and Password do not match"})                
+            }
+            let user = await User.create({firstName,lastName, email, password, tc})
+            if(!user){
+                return res.status(500).json({ status: "failed", message: "Something went wrong while creating the user" });
+            }
+            //Generate Token
+            if(!!user){
+                const token = jwt.sign({userId: user._id}, req.app.get('secretKey'), {expiresIn: '30m'})
+                user.password = undefined
+                if(token){                        
+                    return res.status(201).json({status: "success", message: "User Created Successfully", data: user, token: token})
                 }
             }
         }catch(err){
@@ -36,27 +37,24 @@ const userController = {
     try{
         const {email, password} = req.body
         if(!email || !password){
-            res.json({status: "failed", message: "Please provide required details"})   
+            return res.status(400).json({status: "failed", message: "Please provide required details"})   
         }
         const user = await User.findOne({email})
-        if(!!user){
-            bcrypt.compare(password, user.password, (err, result)=>{
-            if(err){
-                next(err)
-            }else{
-                if(!result){
-                    res.status(401).json({status:"failed", message:"Email or Password does not match", data: {}})
-                }else{
-                    //Generate Token 
-                    const token = jwt.sign({userId: user._id}, req.app.get('secretKey'), {expiresIn: '30m'})
-                    user.password = undefined
-                    if(token){                        
-                        res.status(200).json({status:"success", message:"User Found", data: user, token: token})
-                    }                    
-                }
-         }})}else{
-            res.status(401).json({status:"failed", message:"User Not Found", data: {}})
-         }        
+        if(!user){
+            return res.status(401).json({status:"failed", message:"User Not Found", data: {}})
+        }
+        const bcryptCompare = util.promisify(bcrypt.compare);
+        const result = bcryptCompare(password, user.password)
+
+        if(!result){
+            return res.status(401).json({ status: "failed", message: "Email or Password does not match", data: {} });
+        }
+         //Generate Token 
+        const token = jwt.sign({userId: user._id}, req.app.get('secretKey'), {expiresIn: '30m'})
+        user.password = undefined
+        if(token){                        
+            return res.status(200).json({status:"success", message:"User Found", data: user, token: token})
+        }        
     }catch(err){
         next(err)
     }
